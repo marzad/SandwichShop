ad0e4bfca25d575a8536feb9cbeaceb5f1a9a6eb
marzad <maschatz@gmx.net>
2022-11-13 21:34:00 +0100

    Initial commit

---
+++ src/main/java/de/neuefische/marzad/sandwichshop/SandwichShopApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.marzad.sandwichshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandwichShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandwichShopApplication.class, args);
+	}
+
+}
